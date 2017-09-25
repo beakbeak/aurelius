9e8603fbf4a5845b0b53385367330eb3d8ad0046
Philip Lafleur <7798945+beakbeak@users.noreply.github.com>
2017-09-24 22:01:57 -0700

    basic, unthrottled HTTP streaming

--- main.go
+++ main.go
@@ -4,10 +4,9 @@ package main
 // - HTTP streaming
 //   - need to prevent over-buffering or sending too much data before
 //     it can be played
-//     - for now: fixed-size buffer; rely on client to drain at appropriate speed
-//     - later: throttle encoding speed based on playback speed
-//              (available in packet after av_read_frame())
-//       - *** this will be necessary for silence when paused ***
+//     - throttle encoding speed based on playback speed
+//       (available in packet after av_read_frame())
+// - play silence when nothing is left to play
 // - figure out why timing is wrong when using MKV container
 //   (maybe something to do with time base settings/conversion)
 // - support embedded images
@@ -21,37 +20,69 @@ package main
 
 import (
 	"aurelib"
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 2 {
 		panic("not enough arguments")
 	}
 
 	aurelib.Init()
 
+	http.HandleFunc("/", stream)
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+func stream(
+	w http.ResponseWriter,
+	req *http.Request,
+) {
+	reject := func(format string, args ...interface{}) {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf(format, args...)
+	}
+
 	var resampler *aurelib.Resampler
 	var err error
 	if resampler, err = aurelib.NewResampler(); err != nil {
-		panic(err)
+		reject("failed to create resampler: %v\n", err)
+		return
 	}
 	defer resampler.Destroy()
 
 	options := aurelib.NewSinkOptions()
+	options.Codec = "pcm_s16le"
 
-	var sink *aurelib.FileSink
-	if sink, err = aurelib.NewFileSink(os.Args[len(os.Args)-1], options); err != nil {
-		panic(err)
+	var sink *aurelib.BufferSink
+	if sink, err = aurelib.NewBufferSink("wav", options); err != nil {
+		reject("failed to create sink: %v\n", err)
+		return
 	}
 	defer sink.Destroy()
 
 	var fifo *aurelib.Fifo
 	if fifo, err = aurelib.NewFifo(sink); err != nil {
-		panic(err)
+		reject("failed to create FIFO: %v\n", err)
+		return
 	}
 	defer fifo.Destroy()
+
+	writeBuffer := func() error {
+		buffer := sink.Buffer()
+		if len(buffer) == 0 {
+			return nil
+		}
+
+		count, err := w.Write(sink.Buffer())
+		if count > 0 {
+			sink.Drain(uint(count))
+		}
+		log.Printf("wrote %v bytes\n", count)
+		return err
+	}
 
 	playFile := func(path string) error {
 		var src *aurelib.FileSource
@@ -62,7 +93,7 @@ func main() {
 		defer src.Destroy()
 
 		src.DumpFormat()
-		fmt.Println(src.Tags)
+		log.Println(src.Tags)
 
 		if err := resampler.Setup(
 			src, sink, src.ReplayGain(aurelib.ReplayGainTrack, true),
@@ -88,20 +119,31 @@ func main() {
 					return err
 				}
 			}
+			if err = writeBuffer(); err != nil {
+				return err
+			}
 		}
 		return nil
 	}
 
-	for _, path := range os.Args[1 : len(os.Args)-1] {
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Header().Set("Connection", "close")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Cache-Control", "no-cache, no-store")
+
+	for _, path := range os.Args[1:] {
 		if err := playFile(path); err != nil {
-			fmt.Printf("failed to play '%v': %v\n", path, err)
+			log.Printf("failed to play '%v': %v\n", path, err)
 		}
 	}
 
 	if err = sink.Flush(fifo); err != nil {
-		fmt.Printf("failed to flush sink: %v\n", err)
+		log.Printf("failed to flush sink: %v\n", err)
 	}
 	if err = sink.WriteTrailer(); err != nil {
-		fmt.Printf("failed to write trailer: %v\n", err)
+		log.Printf("failed to write trailer: %v\n", err)
+	}
+	if err = writeBuffer(); err != nil {
+		log.Printf("failed to write buffer: %v\n", err)
 	}
 }
