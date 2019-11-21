a8c2fede86bf67fbe969fca5e960693ad0d689b8
Philip Lafleur <7798945+beakbeak@users.noreply.github.com>
2019-11-21 09:56:11 -0800

    remove hard-coded database prefix

--- database/database.go
+++ database/database.go
@@ -11,34 +11,18 @@ import (
 )
 
 const (
-	prefix    = "/db"
 	playAhead = 10000 * time.Millisecond
 )
-
-var (
-	reDirPath      *regexp.Regexp
-	rePlaylistPath *regexp.Regexp
-	reTrackPath    *regexp.Regexp
-)
-
-func init() {
-	var err error
-	if reDirPath, err = regexp.Compile(`^` + prefix + `/(:?(.*?)/)?$`); err != nil {
-		panic(err)
-	}
-	if rePlaylistPath, err = regexp.Compile(`^` + prefix + `/(.+?\.[mM]3[uU])$`); err != nil {
-		panic(err)
-	}
-	if reTrackPath, err = regexp.Compile(`^` + prefix + `/(.+?)/([^/]+)$`); err != nil {
-		panic(err)
-	}
-}
 
 type Database struct {
 	prefix        string
 	root          string
 	templateProxy util.TemplateProxy
 	playlistCache *FileCache
+
+	reDirPath      *regexp.Regexp
+	rePlaylistPath *regexp.Regexp
+	reTrackPath    *regexp.Regexp
 }
 
 func New(
@@ -58,6 +42,10 @@ func New(
 		root:          rootPath,
 		templateProxy: templateProxy,
 		playlistCache: NewFileCache(),
+
+		reDirPath:      regexp.MustCompile(`^` + prefix + `/(:?(.*?)/)?$`),
+		rePlaylistPath: regexp.MustCompile(`^` + prefix + `/(.+?\.[mM]3[uU])$`),
+		reTrackPath:    regexp.MustCompile(`^` + prefix + `/(.+?)/([^/]+)$`),
 	}
 	return &db, nil
 }
@@ -81,19 +69,19 @@ func (db *Database) ServeHTTP(
 
 	util.Debug.Printf("DB request: %v\n", req.URL.Path)
 
-	if matches := reDirPath.FindStringSubmatch(req.URL.Path); matches != nil {
+	if matches := db.reDirPath.FindStringSubmatch(req.URL.Path); matches != nil {
 		util.Debug.Println("dir request")
 		db.handleDirRequest(matches, w, req)
 		return
 	}
 
-	if matches := rePlaylistPath.FindStringSubmatch(req.URL.Path); matches != nil {
+	if matches := db.rePlaylistPath.FindStringSubmatch(req.URL.Path); matches != nil {
 		util.Debug.Println("playlist request")
 		db.handlePlaylistRequest(matches, w, req)
 		return
 	}
 
-	if matches := reTrackPath.FindStringSubmatch(req.URL.Path); matches != nil {
+	if matches := db.reTrackPath.FindStringSubmatch(req.URL.Path); matches != nil {
 		util.Debug.Println("track request")
 		db.handleTrackRequest(matches, w, req)
 		return
